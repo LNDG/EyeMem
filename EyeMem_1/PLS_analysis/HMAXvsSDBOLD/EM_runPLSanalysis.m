fc3c782712c0c161ea9423a12986772b3b7f6a64
Niels Kloosterman <kloosterman@mpib-berlin.mpg.de>
2022-04-29 16:32:15 +0200

    10bins gazespec

--- EyeMem_1/PLS_analysis/HMAXvsSDBOLD/EM_runPLSanalysis.m
+++ EyeMem_1/PLS_analysis/HMAXvsSDBOLD/EM_runPLSanalysis.m
@@ -165,9 +165,10 @@ corrtype = 'Spearman'; %Spearman Pearson
 %     PREIN = '/Users/kloosterman/gridmaster2012/projectdata/eyemem/variability/ftsource/taskPLS/nanstd_3bins/bin5-bin1/gazespecific'
 %     PREIN = '/Users/kloosterman/gridmaster2012/projectdata/eyemem/variability/ftsource/taskPLS/nanstd_5bins/bin5-bin1/gazespecific'
     
-%     PREIN = '/Users/kloosterman/gridmaster2012/projectdata/eyemem/variability/ftsource/taskPLS/iqr_5bins/gaze-specific/young'
+    PREIN = '/Users/kloosterman/gridmaster2012/projectdata/eyemem/variability/ftsource/taskPLS/iqr_5bins/gaze-specific/young';
     PREIN = '/Users/kloosterman/gridmaster2012/projectdata/eyemem/variability/ftsource/taskPLS/iqr_5bins/non-gazespecific/young';
     PREIN = '/Users/kloosterman/gridmaster2012/projectdata/eyemem/variability/ftsource/taskPLS/iqr_3bins/non-gazespecific/young';
+    PREIN = '/Users/kloosterman/gridmaster2012/projectdata/eyemem/variability/ftsource/taskPLS/iqr_3bins/gaze-specific/young';
     
     disp 'Generate model txt file'
     txtfilename = 'SDbold_vs_HMAX_gazespec_OAvsYA_BfMRIanalysis.txt';
