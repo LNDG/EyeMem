da4758ea496066aaba0e26111db954061783b578
Niels Kloosterman <kloosterman@mpib-berlin.mpg.de>
2022-05-30 16:07:01 +0200

    rolledback ngspec

--- EyeMem_1/PLS_analysis/HMAXvsSDBOLD/EM_pls_SDbold_vs_HMAX_setup.m
+++ EyeMem_1/PLS_analysis/HMAXvsSDBOLD/EM_pls_SDbold_vs_HMAX_setup.m
@@ -24,8 +24,8 @@ memreq = 2000; % in MB
 % analysis settings
 PLStype = 'taskPLS'; % behavPLSvsdprime taskPLS behavPLSvsDDM
 nbins = 5; % no of bins used for Hmax binning
-gazespecificHMAX = 'gaze-specific';
-% gazespecificHMAX = 'non-gazespecific'; 
+% gazespecificHMAX = 'gaze-specific';
+gazespecificHMAX = 'non-gazespecific'; 
 BOLDvar_measure = 'std'; % iqr, std
 bintype = 'fixednbins';   %fixednbins   uniformbinwidth
 removeoutliers = false;
