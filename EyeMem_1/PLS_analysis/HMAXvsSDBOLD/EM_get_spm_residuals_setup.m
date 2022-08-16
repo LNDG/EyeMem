be194092cf8726784660428bfbded705bacff3f5
Niels Kloosterman <kloosterman@mpib-berlin.mpg.de>
2022-08-16 21:49:58 +0200

    Update EM_get_spm_residuals_setup.m

--- EyeMem_1/PLS_analysis/HMAXvsSDBOLD/EM_get_spm_residuals_setup.m
+++ EyeMem_1/PLS_analysis/HMAXvsSDBOLD/EM_get_spm_residuals_setup.m
@@ -78,6 +78,6 @@ if strcmp(backend, 'local')
   cellfun(fun2run, cfglist);
 else
   qsubcellfun(fun2run, cfglist, 'memreq', memreq*1e6, 'timreq', timreq*60, 'stack', 1, ...
-    'StopOnError', true, 'UniformOutput', true, 'backend', backend, 'options', options);
+    'StopOnError', 0, 'UniformOutput', true, 'backend', backend, 'options', options);
 end
 
