2d9b6365851ddd1d78178f5f7d52aec5822ab709
Niels Kloosterman <kloosterman@mpib-berlin.mpg.de>
2022-08-16 21:43:35 +0200

    get residuals

--- EyeMem_1/PLS_analysis/HMAXvsSDBOLD/EM_get_spm_residuals.m
+++ EyeMem_1/PLS_analysis/HMAXvsSDBOLD/EM_get_spm_residuals.m
@@ -1,25 +1,53 @@
-function EM_get_spm_residuals()
-% TODO run without spm_filter
+function EM_get_spm_residuals(cfg)
+% TODO run without spm_filter TODO check timeseries
 nvoi = 5;
 
-for itrial = 1:150
-  cd('sub-27_sd_alltrials_1-001')
-  load SPM.mat
+mripath = cfg.mripath;
+cd(mripath)
+trials = dir('*alltrials_*');
+ntrials = length(trials);
+subjstr = cfg.subjstr;
+outfile = cfg.outfile;
+
+mri = ft_read_mri(sprintf('../%s_sd_alltrials_sd.nii', subjstr));
+mri.dim = [mri.dim nvoi ntrials ]; % reshape after collecting CHECK!
+mri.anatomy = nan(mri.dim);
+
+for itrial = 1:ntrials
+  disp(itrial)
+  cd(trials(itrial).name)
+  load('SPM.mat')
   
-  spm_write_residuals_edit(SPM, 0); % residual ts comes out, 2310 vols
+  if ~ismac % only works on tardis
+    allvols_spm = spm_write_residuals_edit(SPM, 0); % residual ts comes out, 2310 vols
+  end
   
   %select VOIS of interest, load them;
   onset = round(SPM.Sess.U(1).ons); % interest
   volsoi = onset:onset+(nvoi-1);
+  
+  res_vols = dir('Res_*.nii');
   for ivoi = 1:nvoi
-    ft_read_mri()
+    volname = res_vols(volsoi(ivoi)).name;
+    temp = ft_read_mri( volname );
+    mri.anatomy(:,:,:,ivoi,itrial) = temp.anatomy;
   end
   
   % delete all residual vols
+  if ~ismac % only  on tardis
+    delete('Res_*.nii')
+  end
   
-  
-  
-  
-  
+  cd ..
   
 end
+
+mri.anatomy = mri.anatomy(:,:,:,:);
+mri.dim = [mri.dim(1:3) prod(mri.dim(4:5))];
+
+disp(outfile)
+ft_write_mri(outfile, single(mri.anatomy),  'transform', mri.transform, 'dataformat', 'nifti'  )
+
+
+
+% write nifti with 150*5 = 750 residuals, can go to EM_mri_to_ftsource
---
+++ EyeMem_1/PLS_analysis/HMAXvsSDBOLD/EM_get_spm_residuals_setup.m
@@ -0,0 +1,83 @@
+function EM_get_spm_residuals_setup()
+% run from runMIBmeg_analysis
+
+if ismac
+  basepath = '/Users/kloosterman/gridmaster2012/projectdata/eyemem/'; %yesno or 2afc
+  %     backend = 'parfor';
+  backend = 'local';
+  %   backend = 'qsublocal';
+  compile = 'no';
+else
+  basepath = '/home/mpib/kloosterman/projectdata/eyemem/'; %yesno or 2afc
+  backend = 'slurm';
+  %       backend = 'torque';
+  %   backend = 'local';
+  compile = 'no';
+end
+timreq = 600; %in minutes per run
+memreq = 10000; % in MB
+
+nTRpertrial = 1; % 1 for classic LSS
+PREIN = fullfile(basepath, 'variability2', sprintf('%dTRspertrial', nTRpertrial));
+PREINeye = fullfile(basepath, 'preproc', 'eye');
+
+PREOUT = fullfile(PREIN, 'ftsource', 'residuals');
+
+mkdir(PREOUT)
+
+overwrite = 1;
+
+SUBJ= [9, 11:59, 61:69, 71,72, 74:101]; % TODO specify further?
+
+%make cells for each subject, to analyze in parallel
+cfg = [];
+cfg.PREIN = PREIN;
+cfg.PREOUT = PREOUT;
+cfg.nTRpertrial = nTRpertrial;
+cfglist = {};
+
+for isub = 1:length(SUBJ)
+  if SUBJ(isub) == 9
+    subjstr = sprintf('sub-0%d', SUBJ(isub));
+  else
+    subjstr = sprintf('sub-%d', SUBJ(isub));
+  end
+  mripath = fullfile(PREIN, subjstr, 'spm');
+  if SUBJ(isub) < 10
+    cfg.eyefile = fullfile(PREINeye, sprintf('eye_sub-0%d.mat', SUBJ(isub)));
+  else
+    cfg.eyefile = fullfile(PREINeye, sprintf('eye_sub-%d.mat', SUBJ(isub)));
+  end
+  
+  cfg.subjstr = subjstr;
+  cfg.subjno = SUBJ(isub);
+  cfg.mripath = mripath;
+  cfg.outfile = fullfile(PREIN, subjstr, 'beta_series', sprintf('%s_sd_alltrials_residuals.nii', subjstr));
+  if ~exist(cfg.outfile, 'file') || overwrite
+    cfglist{end+1} = cfg;
+  end
+end
+
+% cfglist = cfglist(2)
+% cfglist = cfglist(randsample(length(cfglist),length(cfglist)));
+
+fprintf('Running %s for %d cfgs\n', mfilename, length(cfglist))
+
+if strcmp(backend, 'slurm')
+  options = '-D. -c1'; % --gres=gpu:1
+else
+  options =  '-l nodes=1:ppn=2'; % torque %-q testing or gpu
+end
+
+fun2run = @EM_get_spm_residuals;
+
+setenv('TORQUEHOME', 'yes')
+mkdir('~/qsub'); cd('~/qsub');
+
+if strcmp(backend, 'local')
+  cellfun(fun2run, cfglist);
+else
+  qsubcellfun(fun2run, cfglist, 'memreq', memreq*1e6, 'timreq', timreq*60, 'stack', 1, ...
+    'StopOnError', true, 'UniformOutput', true, 'backend', backend, 'options', options);
+end
+
