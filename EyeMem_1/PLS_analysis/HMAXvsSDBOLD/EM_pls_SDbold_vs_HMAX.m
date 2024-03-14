aadfbc847bacc24141f623706efd837254dd67e0
Niels Kloosterman <kloosterman@mpib-berlin.mpg.de>
2024-03-14 17:06:59 +0100

    mean

--- EyeMem_1/PLS_analysis/HMAXvsSDBOLD/EM_pls_SDbold_vs_HMAX.m
+++ EyeMem_1/PLS_analysis/HMAXvsSDBOLD/EM_pls_SDbold_vs_HMAX.m
@@ -75,7 +75,7 @@ switch gazespecificHMAX
   case 'gaze-specific'
     %     [sortHMAX, sortinds] = sort(data.trialinfo(validtrials,17));  % gazelocked hmax in 17
     %     [sortHMAX, sortinds] = sort(source.trialinfo.HMAX_fix);  % gazelocked hmax in 17
-    bin_variable = source.trialinfo.HMAX_fix_lookregion_max;
+    bin_variable = source.trialinfo.HMAX_fix_lookregion_mean;
 end
 cfg=[];
 cfg.trials = bin_variable > 0;     % only take trials with valid HMAX_fix_lookregion
